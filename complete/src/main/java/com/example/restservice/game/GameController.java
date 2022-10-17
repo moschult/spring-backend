8b5b8d5610891c5d4b1794b0702a77a597b24589
Moritz Schult <moritzschult@gmail.com>
2022-10-17 21:55:54 +0200

    Add get and delete request; not tested yet

--- complete/src/main/java/com/example/restservice/game/GameController.java
+++ complete/src/main/java/com/example/restservice/game/GameController.java
@@ -3,9 +3,12 @@ package com.example.restservice.game;
 import com.example.restservice.Game;
 import com.example.restservice.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping(path = "/api")
@@ -21,6 +24,11 @@ public class GameController {
         return gameRepository.findAll();
     }
 
+    @GetMapping(path + "/{id}")
+    public @ResponseBody Optional<Game> getGameById(@PathVariable Integer id) {
+        return gameRepository.findById(id);
+    }
+
     @PostMapping(path = path)
     public ResponseEntity<Game> addGame(@RequestBody Game game) {
         Game gameDb = new Game();
@@ -33,5 +41,18 @@ public class GameController {
     public ResponseEntity<Game> putGame(@RequestBody Game game) {
         gameRepository.save(game);
         return ResponseEntity.ok(game);
+    }
+
+    /**
+     * Todo: Add check, if variable exists after deletion. Otherwise deletion
+     * was not successfull and a notfound should be returned
+     *
+     * @param id
+     * @return
+     */
+    @DeleteMapping(value = path + "/{id}")
+    public ResponseEntity<HttpStatus> deleteGame(@PathVariable Integer id) {
+        gameRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
